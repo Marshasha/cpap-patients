8cfb910e3e34cb038cc490104466f89adbc65389
Marshasha <mspieldenner@gmx.de>
2022-06-14 14:39:05 +0200

    connexion works

--- src/services/authentication.js
+++ src/services/authentication.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+//const API_URL = 'http://localhost:3090/api/auth/'
 const API_URL = 'http://localhost:3090/api/auth/'
 
 const register = (username, email, password) => {
